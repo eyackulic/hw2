e7ada619acce369904653a28f6585c17d0bd7ed5
eyackulic <62761629+eyackulic@users.noreply.github.com>
2022-02-24 13:18:26 -0700

    updates

--- main.cpp
+++ main.cpp
@@ -20,17 +20,17 @@ int main(int argc, char ** argv){
 //const char *argv;
 //argv = "/Users/ethanyackulic/CLionProjects/INF504/hw2/sample_hw_dataset.fa";
      FASTAreadset_LL linked_list(argv[2]);
-     const char *seq1;
-     const char *seq2;
-     const char *seq3;
-     const char *seq4;
-     const char *seq5;
-
-     seq1 = "CTAGGTACATCCACACACAGCAGCGCATTATGTATTTATTGGATTTATTT";
-     seq2 = "GCGCGATCAGCTTCGCGCGCACCGCGAGCGCCGATTGCACGAAATGGCGC";
-     seq3 = "CGATGATCAGGGGCGTTGCGTAATAGAAACTGCGAAGCCGCTCTATCGCC";
-     seq4 = "CGTTGGGAGTGCTTGGTTTAGCGCAAATGAGTTTTCGAGGCTATCAAAAA";
-     seq5 = "ACTGTAGAAGAAAAAAGTGAGGCTGCTCTTTTACAAGAAAAAGTNNNNNN";
+//     const char *seq1;
+//     const char *seq2;
+//     const char *seq3;
+//     const char *seq4;
+//     const char *seq5;
+//
+//     seq1 = "CTAGGTACATCCACACACAGCAGCGCATTATGTATTTATTGGATTTATTT";
+//     seq2 = "GCGCGATCAGCTTCGCGCGCACCGCGAGCGCCGATTGCACGAAATGGCGC";
+//     seq3 = "CGATGATCAGGGGCGTTGCGTAATAGAAACTGCGAAGCCGCTCTATCGCC";
+//     seq4 = "CGTTGGGAGTGCTTGGTTTAGCGCAAATGAGTTTTCGAGGCTATCAAAAA";
+//     seq5 = "ACTGTAGAAGAAAAAAGTGAGGCTGCTCTTTTACAAGAAAAAGTNNNNNN";
 
 //     Node * result_node;
 //     result_node = linked_list.searchNode(seq1);
