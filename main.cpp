41dcac56cff4a4562d9c5e67ee28d4c78df9b006
eyackulic <62761629+eyackulic@users.noreply.github.com>
2022-02-24 19:17:42 -0700

    updates

--- main.cpp
+++ main.cpp
@@ -23,26 +23,54 @@ cout << "update" << endl;
 FASTAreadset_LL linked_list(argv[2]);
      cout << "update" << endl;
      const char *seq1;
-//     const char *seq2;
-//     const char *seq3;
-//     const char *seq4;
-//     const char *seq5;
+     const char *seq2;
+     const char *seq3;
+     const char *seq4;
+     const char *seq5;
 //
      seq1 = "CTAGGTACATCCACACACAGCAGCGCATTATGTATTTATTGGATTTATTT";
-//     seq2 = "GCGCGATCAGCTTCGCGCGCACCGCGAGCGCCGATTGCACGAAATGGCGC";
-//     seq3 = "CGATGATCAGGGGCGTTGCGTAATAGAAACTGCGAAGCCGCTCTATCGCC";
-//     seq4 = "CGTTGGGAGTGCTTGGTTTAGCGCAAATGAGTTTTCGAGGCTATCAAAAA";
-//     seq5 = "ACTGTAGAAGAAAAAAGTGAGGCTGCTCTTTTACAAGAAAAAGTNNNNNN";
+     seq2 = "GCGCGATCAGCTTCGCGCGCACCGCGAGCGCCGATTGCACGAAATGGCGC";
+     seq3 = "CGATGATCAGGGGCGTTGCGTAATAGAAACTGCGAAGCCGCTCTATCGCC";
+     seq4 = "CGTTGGGAGTGCTTGGTTTAGCGCAAATGAGTTTTCGAGGCTATCAAAAA";
+     seq5 = "ACTGTAGAAGAAAAAAGTGAGGCTGCTCTTTTACAAGAAAAAGTNNNNNN";
 
- //   Node * result_node;
-//    result_node = linked_list.searchNode(seq1);
-//     if(result_node != nullptr) {
-//         cout << "Match found! Sequence: " << result_node->sequence << endl;
-//     }else{
-//         cout << "Not Match Found :(" << endl;
-//     }
+     Node * result_node;
+     Node * result_node2;
+     Node * result_node3;
+     Node * result_node4;
+     Node * result_node5;
+    result_node = linked_list.searchNode(seq1);
+     if(result_node != nullptr) {
+         cout << "Match found! Sequence: " << result_node1->sequence << endl;
+     }else{
+         cout << "Not Match Found :(" << endl;
+     }
+     result_node2 = linked_list.searchNode(seq1);
+     if(result_node2 != nullptr) {
+         cout << "Match found! Sequence: " << result_node2->sequence << endl;
+     }else{
+         cout << "Not Match Found :(" << endl;
+     }
+     result_node3 = linked_list.searchNode(seq1);
+     if(result_node3 != nullptr) {
+         cout << "Match found! Sequence: " << result_node3->sequence << endl;
+     }else{
+         cout << "Not Match Found :(" << endl;
+     }
+     result_node4 = linked_list.searchNode(seq1);
+     if(result_node4 != nullptr) {
+         cout << "Match found! Sequence: " << result_node4->sequence << endl;
+     }else{
+         cout << "Not Match Found :(" << endl;
+     }
+     result_node5 = linked_list.searchNode(seq1);
+     if(result_node5 != nullptr) {
+         cout << "Match found! Sequence: " << result_node5->sequence << endl;
+     }else{
+         cout << "Not Match Found :(" << endl;
+     }
 
-     linked_list.searchNode(seq1);
+//     linked_list.searchNode(seq1);
 //     linked_list.searchNode(seq2);
 //     linked_list.searchNode(seq3);
 //     linked_list.searchNode(seq4);
