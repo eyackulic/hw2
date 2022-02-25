5797c66b808a0b755f49370dfbb409cc09d62fb2
eyackulic <62761629+eyackulic@users.noreply.github.com>
2022-02-24 19:09:37 -0700

    updates

--- main.cpp
+++ main.cpp
@@ -22,27 +22,27 @@ int main(int argc, char ** argv){
 cout << "update" << endl;
 FASTAreadset_LL linked_list(argv[2]);
      cout << "update" << endl;
- //    const char *seq1;
+     const char *seq1;
 //     const char *seq2;
 //     const char *seq3;
 //     const char *seq4;
 //     const char *seq5;
 //
-  //   seq1 = "CTAGGTACATCCACACACAGCAGCGCATTATGTATTTATTGGATTTATTT";
+     seq1 = "CTAGGTACATCCACACACAGCAGCGCATTATGTATTTATTGGATTTATTT";
 //     seq2 = "GCGCGATCAGCTTCGCGCGCACCGCGAGCGCCGATTGCACGAAATGGCGC";
 //     seq3 = "CGATGATCAGGGGCGTTGCGTAATAGAAACTGCGAAGCCGCTCTATCGCC";
 //     seq4 = "CGTTGGGAGTGCTTGGTTTAGCGCAAATGAGTTTTCGAGGCTATCAAAAA";
 //     seq5 = "ACTGTAGAAGAAAAAAGTGAGGCTGCTCTTTTACAAGAAAAAGTNNNNNN";
 
-//     Node * result_node;
-//     result_node = linked_list.searchNode(seq1);
+ //   Node * result_node;
+//    result_node = linked_list.searchNode(seq1);
 //     if(result_node != nullptr) {
 //         cout << "Match found! Sequence: " << result_node->sequence << endl;
 //     }else{
 //         cout << "Not Match Found :(" << endl;
 //     }
 
-//     linked_list.searchNode(seq1);
+     linked_list.searchNode(seq1);
 //     linked_list.searchNode(seq2);
 //     linked_list.searchNode(seq3);
 //     linked_list.searchNode(seq4);
